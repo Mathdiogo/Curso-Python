110c8b02e9a87e258a106f41c76f2efbb988a500
Matheus Diogo Teixeira <131811983+Mathdiogo@users.noreply.github.com>
2024-08-02 10:24:09 -0300

    Add aula 13, the last 3 commits were not captured

---
+++ Sessão 4/aula113.py
@@ -0,0 +1,24 @@
+# Exercícios com funções
+
+# Crie uma função que multiplica todos os argumentos
+# não nomeados recebidos
+# Retorne o total para uma variável e mostre o valor
+# da variável.
+def multiplicar(*args):
+    total = 1 
+    for numero in args:
+        total *= numero
+    return total
+
+multiplicacao =  multiplicar(1, 2, 3, 4, 5)
+print(multiplicacao)
+# Crie uma função fala se um número é par ou ímpar.
+# Retorne se o número é par ou ímpar.
+
+def par_impar(numero):
+    multiplo_de_dois = numero % 2 == 0
+    
+    if multiplo_de_dois:
+        return f'{numero}par'
+    
+    return f'{numero}ímpar'
