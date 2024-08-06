dc77b867e7539a31aaf2a403b42823a334ba7b58
FIXED-TERM Pinto Matheus (VM-CVS/ENG11-LA) <pim2srb@bosch.com>
2024-08-06 09:22:51 -0300

    Add new coment to aula 114

---
+++ Sessão 4/aula114.py
@@ -0,0 +1,21 @@
+"""
+Higher Order Functions - Funções que podem receber e/ou retornar outras funções
+Funções de primeira classe - First-Class Functions - Funções que são tratadas 
+como outros tipos de dados comuns (strings, inteiros, etc...)
+"""
+
+
+def saudacao(msg, nome):
+    return f'{msg}, {nome}!'
+
+
+def executa(funcao, *args):
+    return funcao(*args)
+
+
+print(
+    executa(saudacao, 'Bom dia', 'Luiz')
+)
+print(
+    executa(saudacao, 'Boa noite', 'Maria')
+)
