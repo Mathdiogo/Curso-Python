9ce13e52bbe2f491eab5ecc15e43a9a60d6a3383
Matheus Diogo Teixeira <131811983+Mathdiogo@users.noreply.github.com>
2024-08-28 13:29:58 -0300

    Add aula 118 resolução ex aula 117

---
+++ Sessão 4/aula118.py
@@ -0,0 +1,15 @@
+#Resolução exercício da aula 117
+def Criar_multiplicador(qntd_de_vezes_multiplicar):
+    def multiplicar(numero):
+            return numero * qntd_de_vezes_multiplicar
+    return multiplicar
+
+duplicando = Criar_multiplicador(2)
+triplicando = Criar_multiplicador(3)
+quadruplicando = Criar_multiplicador(4)
+quintuplando = Criar_multiplicador(5)
+
+print('Duplicando 10: ', duplicando(10))
+print('Triplicando 2: ', triplicando(2))
+print('Quadruplicando 5: ', quadruplicando(5))
+print('Quintuplando 10: ', quintuplando(10))
