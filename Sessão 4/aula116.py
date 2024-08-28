41ac09f5a80fcc048a8a008b6b5476281c8783a7
Matheus Diogo Teixeira <131811983+Mathdiogo@users.noreply.github.com>
2024-08-28 10:12:35 -0300

    Updade file aula116

--- Sessão 4/aula116.py
+++ Sessão 4/aula116.py
@@ -10,16 +10,31 @@ Clousure e funções que retornam outras funções
 # print(saudacao1)
 # print(saudacao2)
 
-def criar_saudacao(saudacao, nome):
-    def saudar():
+# def criar_saudacao(saudacao, nome):
+#     def saudar():
+#         return f'{saudacao}, {nome}!'
+#     return saudar()
+
+# saudacao1 = criar_saudacao('Bom dia', 'Matheus')
+# saudacao2 = criar_saudacao('Boa noite', 'Matheus')
+
+
+
+
+# print(saudacao1)
+# print(saudacao2)
+
+def criar_saudacao(saudacao):
+    def saudar(nome):
         return f'{saudacao}, {nome}!'
-    return saudar()
+    return saudar
 
-saudacao1 = criar_saudacao('Bom dia', 'Matheus')
-saudacao2 = criar_saudacao('Boa noite', 'Matheus')
-
-
+falar_bom_dia = criar_saudacao('Bom dia')
+falar_boa_noite = criar_saudacao('Boa noite')
 
 
-print(saudacao1)
-print(saudacao2)
+
+
+for nome in ['Maria', 'Rayssa', 'Matheus', 'João']:
+    print(falar_bom_dia(nome))
+    print(falar_boa_noite(nome))
