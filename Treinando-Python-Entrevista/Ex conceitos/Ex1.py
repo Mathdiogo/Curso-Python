8c514796290dfb99feb542c3c082ccd2649b0e61
Mathdiogo <matheusponte2010@hotmail.com>
2025-01-30 14:17:19 -0300

    atualizando ex1(conceitos)

--- Treinando-Python-Entrevista/Ex conceitos/Ex1.py
+++ Treinando-Python-Entrevista/Ex conceitos/Ex1.py
@@ -1,11 +1,9 @@
 def impar_par():
     print("Digite um número para realizar a verificação ou uma letra para encerrar!")
+    # While True
     while True:
         try:
             numero = input("Digite um número para verificar se é par ou ímpar: ")
-            if numero.isalpha():
-                print("Programa encerrado, caractere digitado não é um número")
-                break
             numero = int(numero)
             if numero % 2 == 0:
                 print("Esse número é par")
